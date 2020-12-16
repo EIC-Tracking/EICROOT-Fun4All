571fd518026c86cb4eaa9f43b106afabfe88c172
Athira Vijayakumar <39921388+Athira-kv@users.noreply.github.com>
2020-12-16 23:38:18 +0530

    Update TrackFastSimEval.cc

--- TrackFastSimEval.cc
+++ TrackFastSimEval.cc
@@ -32,6 +32,9 @@ TrackFastSimEval::TrackFastSimEval(const string &name, const string &filename,
   , _trackmapname(trackmapname)
   , _event(0)
   , _h1d_Delta_mom(nullptr)
+  , h2(nullptr)
+  ,h3(nullptr)
+
 {
 } // TrackFastSimEval::TrackFastSimEval()
 
@@ -43,6 +46,9 @@ int TrackFastSimEval::Init(PHCompositeNode *topNode)
   PHTFileServer::get().open(_outfile_name, "RECREATE");
 
   _h1d_Delta_mom = new TH1D("_h1d_Delta_mom", "#frac{#Delta p}{truth p}", 100, -0.2, 0.2);
+ h2 = new TH1D ("eta_dist","#eta distribution",100,0,4);
+  h3 = new TH1D ("eta_gen"," #eta disrtibution",100, 0,4);
+
 
   return Fun4AllReturnCodes::EVENT_OK;
 } // TrackFastSimEval::Init()
@@ -68,6 +74,19 @@ int TrackFastSimEval::process_event(PHCompositeNode *topNode)
     for (auto truth_itr = range.first; truth_itr != range.second; ++truth_itr) {
       auto g4particle = truth_itr->second;
       if (!g4particle) continue;
+	    
+	      TVector3 truth_mom(g4particle->get_px(), g4particle->get_py(), g4particle->get_pz());
+          TVector3 reco_mom (     track->get_px(),      track->get_py(),      track->get_pz());
+          float theta = acos(g4particle->get_pz()/truth_mom.Mag());
+          float eta;
+          if(theta>0){
+            eta = -log(tan(theta/2));}
+          else{ eta = -(-log(tan((M_PI + theta)/2)));
+          }
+          std::cout<<" theta "<<theta<<" eta "<<eta<<std::endl;
+
+          h3->Fill(theta);
+
 
       // Loop through all the reconstructed particles and find a match; how about std::map?;
       for (auto track_itr = _trackmap->begin(); track_itr != _trackmap->end(); track_itr++) {
@@ -76,6 +95,11 @@ int TrackFastSimEval::process_event(PHCompositeNode *topNode)
 	  std::cout << "ERROR CASTING PARTICLE!" << std::endl;
 	  continue;
 	} //if
+	      
+	 std::cout<<" eta and id "<<track->get_truth_track_id()<<"   "<<track->get_eta()<<std::endl;
+        float track_theta = 2*atan(exp(-track->get_eta()));
+        h2->Fill(track_theta);
+
 
 	// Matching reconstructed particle found, use it and break;
 	if ((track->get_truth_track_id() - g4particle->get_track_id()) == 0) {
@@ -99,6 +123,9 @@ int TrackFastSimEval::End(PHCompositeNode *topNode)
   PHTFileServer::get().cd(_outfile_name);
 
   _h1d_Delta_mom->Write();
+  h2->Write();
+  h3->Write();
+
 
   return Fun4AllReturnCodes::EVENT_OK;
 } // TrackFastSimEval::End()
